4bf99578bc6ce84d959278532bb578135fdb0e4b
amir-hossein-moqimi <amirmoqim184@gmail.com>
2022-03-07 01:02:58 +0330

    sign up models

--- FLOGRAM/accounts/models.py
+++ FLOGRAM/accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class CreateUser(models.Model):
+    class TYPE(models.TextChoices):
+        Influencer = 'Influencer'
+        Manufacture = 'Manufacture'
+        Sponsor = 'Sponsor'
+    username = models.CharField(max_length=20)
+    Instagram = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    email = models.EmailField(max_length=20)
+    type = models.CharField(max_length=11, choices=TYPE.choices, default=TYPE.Influencer)
+    def __str__(self):
+        return self.username
--- FLOGRAM/accounts/views.py
+++ FLOGRAM/accounts/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from ast import If
+from django.shortcuts import render,redirect
+from . import forms,models
 # Create your views here.
 def login(request):
     return render(request, 'login.html')
 def signup(request):
+    # print(request.POST)
+    if request.method == 'POST':
+        form = forms.CreationForm(request.POST)
+        if form.is_valid():
+            print("--------------------------------")
+            #not validated completely
+            ins = form.save(commit=False)
+            ins.username = request.POST.get('Username')
+            ins.Instagram = request.POST.get('Instagram')
+            ins.password = request.POST.get('Password')
+            ins.email = request.POST.get('Email')
+            ins.save()
+    else:
+        form = forms.CreationForm()
     return render(request, 'signup.html')
