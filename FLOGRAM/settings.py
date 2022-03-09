5c494db7fa9ec53e7fdd4dd6713c3bdad631ee8d
amir-hossein-moqimi <amirmoqim184@gmail.com>
2022-03-09 20:58:12 +0330

    deploy

--- FLOGRAM/settings.py
+++ FLOGRAM/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,6 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 # STATIC_ROOT = os.path.join(BASE_DIR , 'static')
 STATICFILES_DIRS = (
