27e1f60cb75ac5b26d24d8ec9c68c76a6b1170b8
amir-hossein-moqimi <amirmoqim184@gmail.com>
2022-03-14 20:41:09 +0330

    sign up form connected to db

--- accounts/models.py
+++ accounts/models.py
@@ -6,10 +6,10 @@ class CreateUser(models.Model):
         Influencer = 'Influencer'
         Manufacture = 'Manufacture'
         Sponsor = 'Sponsor'
-    username = models.CharField(max_length=20)
-    Instagram = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
-    email = models.EmailField(max_length=20)
+    username = models.CharField(max_length=50)
+    Instagram = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
     type = models.CharField(max_length=11, choices=TYPE.choices, default=TYPE.Influencer)
     def __str__(self):
         return self.username
--- accounts/views.py
+++ accounts/views.py
@@ -1,22 +1,27 @@
 from ast import If
+from distutils import errors
 from django.shortcuts import render,redirect
 from . import forms,models
 # Create your views here.
 def login(request):
     return render(request, 'login.html')
 def signup(request):
-    # print(request.POST)
     if request.method == 'POST':
         form = forms.CreationForm(request.POST)
+        print(f'request.POST : {request.POST}')
         if form.is_valid():
-            print("--------------------------------")
             #not validated completely
             ins = form.save(commit=False)
-            ins.username = request.POST.get('Username')
+            ins.username = request.POST.get('username')
             ins.Instagram = request.POST.get('Instagram')
-            ins.password = request.POST.get('Password')
-            ins.email = request.POST.get('Email')
+            ins.password = request.POST.get('password')
+            ins.email = request.POST.get('email')
+            ins.type = request.POST.get('type')
             ins.save()
+            return redirect('accounts:loginpage')
+        else:
+            print(f'form.errors : {form.errors.as_data()}')
+            return render(request, 'signup.html', {'haserror' : True, 'errors' : form.errors})
     else:
         form = forms.CreationForm()
-    return render(request, 'signup.html')
+        return render(request, 'signup.html', {'haserror' : False})
