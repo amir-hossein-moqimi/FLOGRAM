2021e8aaef36b3e63055e265b99fbbfa2882e3eb
amir-hossein-moqimi <amirmoqim184@gmail.com>
2022-06-04 23:42:03 +0430

    final

--- FLOGRAM/settings.py
+++ FLOGRAM/settings.py
@@ -134,4 +134,4 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'djangoinstabot@gmail.com'
 EMAIL_HOST_PASSWORD = 'insta1234'
-CURRENT_DOMAIN = 'http://127.0.0.1:8000/'
+CURRENT_DOMAIN = 'https://flogram.herokuapp.com/'
